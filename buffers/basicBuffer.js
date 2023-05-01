234afe1fac7c351bd5c834d58b3dc0064439d279
atulya-kairati <atulyakairati@gmail.com>
2023-05-01 17:54:20 +0530

    basic buffers

---
+++ buffers/basicBuffer.js
@@ -0,0 +1,25 @@
+// Buffer is available globaly but it is recomended to require it.
+const { Buffer } = require('buffer')
+
+// Allocating a buffer
+const buffer1 = Buffer.alloc(16)
+
+// Filling a buffer
+//It will only fill upto its size, rest will be ignored
+buffer1.write("Bing Chlling 12_This will be ignored")
+
+// by default, values will be printed in hex format 
+console.log(buffer1)
+
+// This will show them in decimal
+console.log(buffer1.toJSON())
+
+
+// buffer can be created from a string or array
+// Array values must be in [0, 255] or they will be truncated to fit that range
+const buffer2 = Buffer.from([66, 105, 110, 103])
+
+// We need to specify correct encoding to correct string value
+// In this case we need utf-8
+console.log(buffer2.toString('utf-8'))
+console.log(buffer2.toString('utf16le'))
---
+++ buffers/readme.md
@@ -0,0 +1,9 @@
+### Buffer
+> [Buffer Docs](https://nodejs.org/api/buffer.html)
+
+- It is a container of fixed size in memory used to handle raw binary data.
+- JS was not equipped to handle binary data so Buffers were introduced in node. Since it needs to deal with binary data on a server.
+- Buffers store data as a array of bytes. 
+- And each value must be >=0 and <= 255 (>=0x0 and <=0xff).
+- Values are stored in hexadecimal format.
+
