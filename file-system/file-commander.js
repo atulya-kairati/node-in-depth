3e571516a4e08a81cca9be339086979b56af7a47
atulya-kairati <atulyakairati@gmail.com>
2023-05-10 14:35:05 +0530

    I went full retard

--- file-system/file-commander.js
+++ file-system/file-commander.js
@@ -1,10 +1,14 @@
 const fs = require('fs/promises');
 
 const commands = {
-    CREATE_FILE: 'create a file',
+    CREATE_FILE: 'create a file', // create a file <path>
+    DELETE_FILE: 'delete file', // delete file <path>
+    RENAME_FILE: 'rename file', // rename file <old path> to <new path>
+    ADD_TO_FILE: 'append to', // append to <path> <content>
 };
 
 
+// this where the program starts
 (async () => {
     // watch the command file
     const watcher = fs.watch('./command.txt');
@@ -34,7 +38,7 @@ async function commandReader(fileHandler) {
     const fileSize = (await fileHandler.stat()).size;
 
     // don't read empty file
-    if(fileSize === 0) return;
+    if (fileSize === 0) return;
 
     // allocating a buffer of size = fileSize
     const buffer = Buffer.alloc(fileSize);
@@ -64,20 +68,105 @@ async function commandReader(fileHandler) {
  */
 function commandExecutor(command) {
 
-    const commander = new Commander()
+    const commander = new Commander();
 
-    if(command.includes(commands.CREATE_FILE)){
+    // file creation
+    if (command.includes(commands.CREATE_FILE)) {
+        // extract path and pass it to createFile method of commander
         const path = command.substring(commands.CREATE_FILE.length).trim();
-        commander.createFile(path)
+        commander.createFile(path);
+
+        return;
     }
-    
+
+    // file deletion
+    if (command.includes(commands.DELETE_FILE)) {
+        // extract path and pass it to createFile method of commander
+        const path = command.substring(commands.DELETE_FILE.length).trim();
+        commander.deleteFile(path);
+
+        return;
+    }
+
+    // renaming file
+    if (command.includes(commands.RENAME_FILE)) {
+        // extract path and pass it to createFile method of commander
+        const [oldPath, newPath] = command.substring(commands.RENAME_FILE.length).trim().split(' to ');
+        commander.renameFile(oldPath, newPath);
+
+        return;
+    }
+
+    // appending to file
+    if (command.includes(commands.ADD_TO_FILE)) {
+        // extract path and pass it to createFile method of commander
+        const [path, ...content] = command.substring(commands.ADD_TO_FILE.length).trimStart().split(' ');
+        // Warn: getting content like this is not performant
+        commander.appendToFile(path, content.join(' '));
+
+        return;
+    }
+
 }
 
+/**
+ * Class contains all the methods corresponding to the commands
+ */
 class Commander {
     /**
+     * Creates a file with given path
      * @param {String} path 
      */
-    async createFile(path){
-        (await fs.open(path, 'w')).close()
+    async createFile(path) {
+
+        try {
+            // opening file with w command will create file if it doesn't exist
+            // wx -> Like 'w' but fails if the path exists.
+            (await fs.open(path, 'wx')).close() // we close the  handle ASAP
+        } catch (error) {
+            console.log(`File already exists at ${path}`);
+        }
+    }
+
+
+    /**
+     * 
+     * @param {String} path 
+     */
+    async deleteFile(path) {
+
+        try {
+            // unlink removes any symlinks and deletes files
+            await fs.unlink(path)
+        } catch (err) {
+            console.log(`File not found at ${path}`);
+        }
+    }
+
+    /**
+     * @param {String} oldPath 
+     * @param {String} newPath 
+     */
+    async renameFile(oldPath, newPath) {
+        try {
+            await fs.rename(oldPath, newPath)
+        } catch (error) {
+            console.log(`No file found at ${oldPath}`);
+        }
+    }
+
+    /**
+     * @param {String} path 
+     * @param {String} content 
+     */
+    async appendToFile(path, content) {
+        try {
+            // a -> Open file for appending. The file is created if it does not exist.
+            const fileHandler = await fs.open(path, 'a')
+            await fileHandler.appendFile(content)
+            fileHandler.close()
+        } catch (error) {
+            console.log(`File doesn't exist ${path}`);
+        }
     }
 }
